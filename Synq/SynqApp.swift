261b4eaed557ec06f8d9bbb5005f782ad78c131f
Martin Flischman <martincodes@icloud.com>
2025-05-18 13:27:47 +0200

    Initial Commit

---
+++ Synq/SynqApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynqApp.swift
+//  Synq
+//
+//  Created by Martin Flischman on 2025/05/18.
+//
+
+import SwiftUI
+
+@main
+struct SynqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
